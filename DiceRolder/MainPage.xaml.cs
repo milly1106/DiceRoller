1d0dc400e4f48ed02cfd45b87b0459be796cad63
emily <emily@teste.com>
2024-04-09 12:27:05 -0300

    Correcao atividade

---
+++ DiceRolder/MainPage.xaml.cs
@@ -0,0 +1,25 @@
+﻿using Microsoft.Maui.Controls.PlatformConfiguration.iOSSpecific;
+using Microsoft.Maui.Controls.Shapes;
+
+namespace DiceRolder
+{
+    public partial class MainPage : ContentPage
+    {
+        Random random = new Random();
+        public MainPage()
+        {
+            InitializeComponent();
+        }
+        public virtual int Next { get; }
+
+        private void RollDiceButton_Clicked(object sender, EventArgs e)
+        {
+            int selectedSides = Convert.ToInt32(SidesPicker.SelectedItem);
+            int drawnNumber = random.Next(1, selectedSides + 1);
+            DADOS.Text = drawnNumber.ToString();
+       
+        }
+      
+        
+    }
+}
